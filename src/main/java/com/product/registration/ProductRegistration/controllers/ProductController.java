a42c281771564040ee26b49164946321138e3aef
byionline <10815566+byionline@users.noreply.github.com>
2020-08-04 08:58:05 +0530

    Inject ProductRepository to Controller

--- src/main/java/com/product/registration/ProductRegistration/controllers/ProductController.java
+++ src/main/java/com/product/registration/ProductRegistration/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.product.registration.ProductRegistration.controllers;
 
 import com.product.registration.ProductRegistration.models.Product;
+import com.product.registration.ProductRegistration.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
+    //inject Product Repository into Controller
+    @Autowired
+    private ProductRepository productRepository;
     // Get List of Products
     @GetMapping
     public List<Product> list(){
-        List<Product> products = new ArrayList<>();
-        return  products;
+//        List<Product> products = new ArrayList<>();
+//        return  products;
+        return productRepository.findAll();
     }
     // Get Product by id
     @GetMapping("/{id}")
     public Product get(@PathVariable("id") long id){
-        return new Product();
+//        return new Product();
+        return productRepository.getOne(id);
     }
 }
