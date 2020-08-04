94d07473cd25584e1724a2ec24f18af6c668df5e
byionline <10815566+byionline@users.noreply.github.com>
2020-08-04 05:34:24 +0530

    Add Product Attributes

--- src/main/java/com/product/registration/ProductRegistration/models/Product.java
+++ src/main/java/com/product/registration/ProductRegistration/models/Product.java
@@ -1,4 +1,80 @@
 package com.product.registration.ProductRegistration.models;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class Product {
+    // Product Attributes
+    private String serialNumber;
+    private String itemName;
+    private BigDecimal purchasePrice;
+    private Date purchaseDate;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhone;
+    private String customerAddress;
+    
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+    
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+    
+    public String getItemName() {
+        return itemName;
+    }
+    
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+    
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+    
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+    
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+    
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+    
+    public String getCustomerName() {
+        return customerName;
+    }
+    
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+    
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+    
+    public String getCustomerPhone() {
+        return customerPhone;
+    }
+    
+    public void setCustomerPhone(String customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+    
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+    
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
 }
