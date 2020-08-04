72a479eca96d4497b883697e8e57bac729ff993a
byionline <10815566+byionline@users.noreply.github.com>
2020-08-04 05:44:18 +0530

    List All Products Endpoint

--- src/main/java/com/product/registration/ProductRegistration/controllers/ProductController.java
+++ src/main/java/com/product/registration/ProductRegistration/controllers/ProductController.java
@@ -1,4 +1,19 @@
 package com.product.registration.ProductRegistration.controllers;
 
+import com.product.registration.ProductRegistration.models.Product;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+    @GetMapping
+    public List<Product> list(){
+        List<Product> products = new ArrayList<>();
+        return  products;
+    }
 }
