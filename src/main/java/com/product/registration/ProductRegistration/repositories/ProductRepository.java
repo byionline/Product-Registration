eaaf25c850c66e4c1bdaae0fa2dcc2be7f75c125
byionline <10815566+byionline@users.noreply.github.com>
2020-08-04 08:45:55 +0530

    Configure ProductRepository

--- src/main/java/com/product/registration/ProductRegistration/repositories/ProductRepository.java
+++ src/main/java/com/product/registration/ProductRegistration/repositories/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.product.registration.ProductRegistration.repositories;
 
-public interface ProductRepository {
+import com.product.registration.ProductRegistration.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
